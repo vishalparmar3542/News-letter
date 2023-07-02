a366fb2c91e07f4f5e8c4b01ab9fdf5ca94e04cf
vishal <vishalparmar3542@gmail.com>
2023-07-02 22:06:16 +0530

    first commit

--- app.js
+++ app.js
@@ -7,59 +7,56 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.get("/", (req, res) => {
-    res.sendFile(__dirname+"/index.html");
-})
+  res.sendFile(__dirname + "/index.html");
+});
 app.post("/", (req, res) => {
-    const fname = req.body.Firstname;
-    const lname = req.body.Lastname;
-    const email = req.body.email;
-    const data = {
-        members: [
-            {
-                email_address: email,
-                status: "subscribed",
-                merge_fields: {
-                    FNAME: fname,
-                    LNAME: lname
-                }
-            }
-        ]
-    };
-    const auth_id = process.env.AUTH_TOKEN;
-    console.log(auth_id);
-    const JSONdata = JSON.stringify(data);
-    const url = `https://us13.api.mailchimp.com/3.0/lists/05283b704c`;
-    const options = {
-        method: "POST",
-        auth: `VISHAL:${auth_id}`
-    };
-  const request= https.request(url, options, (response) =>
-    {
-        response.on("data", (data) => {
-            console.log(JSON.parse(data));
-            if (response.statusCode==200) {
-                res.sendFile(__dirname + "/success.html");
-               // res.send(`${data}`);
-                //res.send(`${data} id ${auth_id}`);
+  const fname = req.body.Firstname;
+  const lname = req.body.Lastname;
+  const email = req.body.email;
+  const message = req.body.message;
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: fname,
+          LNAME: lname,
+        },
+        contact: {
+          address1: message,
+        },
+      },
+    ],
+  };
+  const auth_id = process.env.AUTH_TOKEN;
+  console.log(auth_id);
+  const JSONdata = JSON.stringify(data);
+  const url = `https://us13.api.mailchimp.com/3.0/lists/05283b704c`;
+  const options = {
+    method: "POST",
 
-            } else {
-               res.sendFile(__dirname+"/faliure.html");
-            // res.send(`${data} id ${auth_id}`);
-
-                
-            }
-        });
+  auth: `VISHAL:${auth_id }`,
+  };
+  const request = https.request(url, options, (response) => {
+    response.on("data", (data) => {
+      console.log(JSON.parse(data));
+      if (response.statusCode == 200) {
+        res.sendFile(__dirname + "/success.html");
+        // res.send(`${data}`);
+        //res.send(`${data} id ${auth_id}`);
+      } else {
+        res.sendFile(__dirname + "/faliure.html");
+        // res.send(`${data} id ${auth_id}`);
+      }
+    });
   });
-    request.write(JSONdata);
-    request.end();
-    
-})
-
-app.listen(process.env.PORT || 3000, () => {
-    console.log("started");
+  request.write(JSONdata);
+  request.end();
 });
 
-
-
+app.listen(process.env.PORT || 3001, () => {
+  console.log("started");
+});
 
 //id:05283b704c
